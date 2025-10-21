3e25563dcf1f37805a6053fbfe089a2200a6a846
TheRisingStorm <ppavlov2612@gmail.com>
2025-10-21 21:14:23 +0200

    Обновлено решение задачи 2.4.4 #13

--- Code/2.4.4/Задача 13.py
+++ Code/2.4.4/Задача 13.py
@@ -16,26 +16,23 @@ Diametr = tuple()
 while len(Weight) !=10 and len(Diametr) != 10:
     Weight+=(r.randint(10, 30),)
     Diametr+=(r.randint(10, 30),)
-    
+
 D = UserInput('Введите число D: ', int)
 W = UserInput('Введите число W: ', int)
 
 Output1 = []
 Output2 = []
 
-for i in range(len(Diametr)-1):
-    for j in range(len(Diametr)-1):
-        exp1 = Diametr[i] == Diametr[j] - D
-        exp2 = Diametr[i] - D == Diametr[j]
-        exp3 = Weight[i] == Weight[j] - W
-        exp4 = Weight[i] - W == Weight[j]
-        if (exp1 or exp2) and (exp3 or exp4) and i != j:
+for i in range(len(Diametr)):
+    for j in range(i+1, len(Diametr)):
+        exp1 = abs(Diametr[i] - Diametr[j]) <= D
+        exp2 = abs(Weight[i] - Weight[j]) <= W
+        if exp1 and exp2: 
             Output1.append([Diametr[i], Weight[i]])
             Output2.append([Diametr[j], Weight[j]])
 for w in range(len(Output1)-1):
     print(f'''Удовлетворяющая пара №{w+1}:
     Вес1: {Output1[w][1]}, Диаметр1: {Output1[w][0]}
     Вес2: {Output2[w][1]}, Диаметр2: {Output2[w][0]}
-    
 ''')
 input('Нажмите ENTER, чтобы выйти.')
