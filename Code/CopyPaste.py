d7cc6a67da9d417b481f85ec7f1a98ae26f14680
TheRisingStorm <ppavlov2612@gmail.com>
2025-11-04 20:55:15 +0200

    CopyPaste file for non-VSC code-writing updated

--- Code/CopyPaste.py
+++ Code/CopyPaste.py
@@ -1,12 +1,22 @@
-print('''Эта программа
-''')
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Mods')))
-from custom_assertions import *
+# всё что идёт для принта - для импорта модуля)
+current_file = os.path.abspath(__file__) #текущий путь
+repo_root = os.path.abspath(os.path.join(current_file, "..", "..", "..")) #Путь от текущей папки в корневую
+mods_path = os.path.join(repo_root, "Mods") # Путь к папке с модулями
+if mods_path not in sys.path: #Добавление в sys.path
+    sys.path.insert(0, mods_path)
+try:
+    from custom_assertions import *
+except ImportError as e:
+    print(f"Модуль custom_assertions не найден: {e}")
+    exit()
+
+print('''"Эта программа
+''')
 
 
 
-
-
+print(f'''
+''')
 input('\nНажмите ENTER, чтобы выйти.')
