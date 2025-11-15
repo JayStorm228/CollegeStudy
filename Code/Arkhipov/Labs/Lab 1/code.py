2da2841d3d88802dc9f66bdfad691314918e8690
TheRisingStorm <ppavlov2612@gmail.com>
2025-11-15 13:07:52 +0200

    Lab 1 Arkhipov

---
+++ Code/Arkhipov/Labs/Lab 1/code.py
@@ -0,0 +1,7 @@
+import math as m
+
+x,y,z = 1.825,18.225,3.298
+Accuracy = 4
+y = round(abs(x**(y/x))- (y/x)**(1/3), Accuracy)
+u = round((y-x)*( ( (y-x)/(y-x) )/(1+(y-x)**2) ), Accuracy)
+print(y, u, sep='\n')
