f01106b18767ec3e2e52b27f8d0f55b5b00f9df7
TheRisingStorm <ppavlov2612@gmail.com>
2025-11-28 22:09:48 +0200

    Обновление блок-схем

--- Code/Arkhipov/2.4.4/Ex 11.py
+++ Code/Arkhipov/2.4.4/Ex 11.py
@@ -1,13 +1,21 @@
-print('''Эта программа попытается удалить первые попадающиеся четыре нулевых значения из списка
-''')
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Mods')))
-from custom_assertions import *
 
-Array = CreateList(UserInput('Введите размер списка: ', int), float)
+# всё что идёт для принта - для импорта модуля)
+current_file = os.path.abspath(__file__)
+repo_root = os.path.abspath(os.path.join(current_file, "..", "..", ".."))
+mods_path = os.path.join(repo_root, "Mods")
+if mods_path not in sys.path:
+    sys.path.insert(0, mods_path)
+try:
+    from custom_assertions import *
+except ImportError as e:
+    print(f"Модуль custom_assertions не найден: {e}")
+    exit()
+
+Array = CreateList(UserInput("Введите размер списка: ", int), float)
 for w in range(3):
     Array.remove(0)
-print(f'Изменённый список: {Array}')
+print(f"Изменённый список: {Array}")
 
-input('Нажмите ENTER, чтобы выйти.')
+input("Нажмите ENTER, чтобы выйти.")
